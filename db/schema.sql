03e709a70a91b8032cf876b020e169410fd114c9
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-07 23:28:41 -0800

    added recomended schema from readme

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,32 @@ CREATE DATABASE credentials_db;
 USE credentials_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   name VARCHAR(30),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT
+  salary DECIMAL(10, 3),
+  department_id INT,
+  PRIMARY KEY(id)
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
-)
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (manager_id),
+    REFERENCES role(id)
+);
+
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
