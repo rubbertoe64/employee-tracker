45c12544a936cde82fda6503f3f5d72beabf9f14
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-08 12:53:44 -0800

    removed express code, made functs for required criteria

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const mysql = require('mysql2');
-const consoletable = require('console.table')
+const cTable = require('console.table')
 const inquirer = require('inquirer')
 
 
 const PORT = process.env.PORT || 3002;
-const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -29,12 +25,114 @@ function init() {
             type: "list",
             name: "init",
             message: "What would you like to do?",
-            choices: ["View All Departments","View All Employees","View All Roles", "Add Employee", "Update Employee Role",  "Add Roles", "Add Department", "Quit"]
+            choices: ["View All Departments","View All Employees","View All Roles", "Add Employee", "Update Employee Role",  "Add Role", "Add Department", "Quit"]
 
         }
     ])
     .then((ans) => {
-
+      switch (ans.init) {
+        case "View All Departments":
+          viewDepartments();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
+        case "View All Employees":
+          viewEmployees();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+      
+        default:
+          break;
+      }
     })
 }
 
+function viewDepartments(){
+  db.query("SELECT * FROM departments", 
+  function(error, res) {
+    if(error) throw error;
+    console.table(res);
+    init();
+  })
+}
+
+function viewRoles(){
+  db.query("SELECT * FROM roles", 
+  function(error, res) {
+    if(error) throw error;
+    console.table(res);
+    init();
+  })
+}
+
+function viewEmployees(){
+  db.query("SELECT * FROM employees", 
+  function(err, res) {
+    if(err) throw err;
+    console.table(res);
+    init();
+  })
+}
+
+function addDepartment(){
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "newDep",
+      message: "Insert Department you want to add."
+    }
+    
+  ])
+  .then((ans) => {
+    console.log(ans);
+    db.query("INSERT INTO departments SET ?", {name: ans.newDep}),
+    
+      db.query("SELECT * FROM departments", 
+
+        function(err, res) {
+          if(err) throw err;
+          console.table(res);
+          init();
+      })
+    
+  })
+}
+
+function addRole(){
+inquirer.prompt([
+    {
+      type: "input",
+      name: "newRole",
+      message: "Insert Role you want to add."
+    }
+    
+  ])
+  .then((ans) => {
+    console.log(ans);
+    db.query("INSERT INTO roles SET ?", {name: ans.newRole}),
+    
+      db.query("SELECT * FROM roles", 
+      
+        function(err, res) {
+          if(err) throw err;
+          console.table(res);
+          init();
+      })
+    
+  })
+}
+
+function addEmployee(){
+  
+}
+
+init();
